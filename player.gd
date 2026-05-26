3adf1f42213b200f11b81360740bde4ce43e7fea
Ivan Kozlov <ivaan.kozlov@gmail.com>
2026-05-25 21:08:30 -0700

    Initial

---
+++ player.gd
@@ -0,0 +1,45 @@
+extends Area2D
+signal hit
+
+@export var speed = 400
+var screen_size
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	velocity.x = int(Input.is_action_pressed("move_right")) \
+		- int(Input.is_action_pressed("move_left"))
+	velocity.y = int(Input.is_action_pressed("move_down")) \
+		- int(Input.is_action_pressed("move_up"))
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
